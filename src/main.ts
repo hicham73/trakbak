34ff96cee85e1269e644e1fae83cbed18c47858e
Wahbi <hwahbi@dxc.com>
2020-08-24 08:25:16 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3001,'localhost');
+  await app.listen(3001,'10.128.0.2');
 }
 bootstrap();
